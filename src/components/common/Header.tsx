9482c44b61a164057ed98652432c47c26a490cd3
oganessone718 <oganessone718@gmail.com>
2025-07-20 23:45:25 +0900

    fix: add spacer

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -10,12 +10,16 @@ const Header: React.FC<HeaderProps> = ({
   right = null,
 }) => {
   return (
+    <div>
       <div className="fixed w-full top-0 z-50 flex justify-between items-center h-[56px] px-[32px] py-[16px] border-b-[1px] border-b-gray-300 bg-white gap-[12px]">
         <div className="flex flex-1 gap-[12px] items-center">
           {left}
           {center}
         </div>
         <div className="flex">{right}</div>
+      </div>
+      {/* spacer */}
+      <div className="h-[56px]"></div>
     </div>
   );
 };
