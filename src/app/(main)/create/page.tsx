9e28188c5784c25ed5ec739eb27859e473590645
oganessone718 <oganessone718@gmail.com>
2025-07-21 00:11:30 +0900

    fix: add header component

--- src/app/(main)/create/page.tsx
+++ src/app/(main)/create/page.tsx
@@ -1,7 +1,13 @@
+import Header from "@/components/common/Header";
+import BackButton from "@/components/common/Button/BackButton";
+
 const Create = () => {
   return (
-    <div className="flex flex-col bg-primary-50">
-      CREATE
+    <div className="flex flex-col">
+      <Header
+        left={<BackButton iconType="x" />}
+        center={<span className="text-lg font-medium">To.Day 작성</span>}
+      />
     </div>
   );
 };
