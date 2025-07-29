d749e2dac917f3d82101c40c0eae1745156c4d8d
oganessone718 <oganessone718@gmail.com>
2025-07-30 04:06:35 +0900

    fix: make bottom bar generic

--- src/components/features/create/CreateSettingBottomBar.tsx
+++ src/components/features/create/CreateSettingBottomBar.tsx
@@ -1,21 +1,21 @@
 "use client";
 
-import useTmpTodayStore from "@/store/useTmpTodayStore";
 import Button from "@/components/common/button/Button";
 
-const CreateSettingsBottomBar = () => {
-  const { tmpToday } = useTmpTodayStore();
+interface CreateSettingsBottomBarProps {
+  buttonText: string;
+  onClick: () => void;
+}
+
+const CreateSettingsBottomBar = ({
+  buttonText,
+  onClick,
+}: CreateSettingsBottomBarProps) => {
 
   return (
     <div>
       <div className="flex justify-end border-t-[1px] border-gray-300 bg-white fixed bottom-0 w-full px-[16px] py-[10px] gap-[12px] z-50">
-        <Button
-          type="submit"
-          text="등록하기"
-          onClick={() => {
-            console.log(tmpToday);
-          }}
-        />
+        <Button text={buttonText} onClick={onClick} />
       </div>
       {/* spacer */}
       <div className="h-[57px]"></div>
