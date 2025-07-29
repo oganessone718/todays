0e47c8989f2c5971da98d4714e4f7dc9aeb2f31c
oganessone718 <oganessone718@gmail.com>
2025-07-29 20:35:27 +0900

    feat: impl input bar component

---
+++ src/components/common/input/InputBar.tsx
@@ -0,0 +1,47 @@
+import React, { ChangeEvent, KeyboardEvent } from "react";
+
+export interface InputBarProps {
+  inputText: string;
+  onChange: (value: string) => void;
+  leftIcon?: React.ReactNode;
+  placeholder?: string;
+  RightIcon?: React.ReactNode;
+  onEnterPress?: () => void;
+}
+
+const InputBar = ({
+  inputText,
+  onChange,
+  leftIcon = null,
+  placeholder = "입력해주세요.",
+  RightIcon = null,
+  onEnterPress = () => {},
+}: InputBarProps) => {
+  const handleValueChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    onChange(inputValue);
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onEnterPress();
+    }
+  };
+
+  return (
+    <div className="select-none flex flex-row items-center px-[16px] py-[8px] gap-[8px] bg-gray-100 rounded-[15px]">
+      {leftIcon}
+      <input
+        type="text"
+        value={inputText}
+        placeholder={placeholder}
+        onChange={handleValueChange}
+        onKeyDown={handleKeyDown}
+        className={`flex-1 focus:outline-none text-[16px] font-medium`}
+      />
+      {RightIcon}
+    </div>
+  );
+};
+
+export default InputBar;
