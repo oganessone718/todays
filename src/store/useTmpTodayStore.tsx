c167f13a446e7f7428238e017e1957386872602e
oganessone718 <oganessone718@gmail.com>
2025-07-29 16:43:36 +0900

    feat: implement tmp today store

---
+++ src/store/useTmpTodayStore.tsx
@@ -0,0 +1,53 @@
+import { create } from "zustand";
+
+export interface TmpTodayValues {
+  id?: number;
+  title: string;
+  content: string;
+  date: string;
+  emojiUrl: string;
+  mentions: number[];
+  tags: number[];
+  visibles: number[];
+}
+
+interface TmpTodayStore {
+  tmpToday: TmpTodayValues;
+  setTmpToday: (tmpToday: TmpTodayValues) => void;
+}
+
+const useTmpTodayStore = create<TmpTodayStore>((set) => ({
+  tmpToday: {
+    title: "",
+    emojiUrl: "",
+    content: "",
+    date: new Date().toISOString().split("T")[0],
+    mentions: [],
+    tags: [],
+    visibles: [],
+  },
+
+  setTmpToday: (updates) =>
+    set((state) => ({
+      ...state,
+      tmpToday: {
+        ...state.tmpToday,
+        ...updates,
+      },
+    })),
+
+  resetTmpToday: () =>
+    set({
+      tmpToday: {
+        title: "",
+        emojiUrl: "",
+        content: "",
+        date: "",
+        mentions: [],
+        tags: [],
+        visibles: [],
+      },
+    }),
+}));
+
+export default useTmpTodayStore;
