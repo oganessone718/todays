d7b1f46dccf5151665a18b630c81670f042004af
oganessone718 <oganessone718@gmail.com>
2025-08-03 01:18:57 +0900

    feat: impl user select row

---
+++ src/components/common/user/UserSelectRow.tsx
@@ -0,0 +1,47 @@
+import type { User } from "@prisma/client";
+import UserRow from "./_atomic/UserRow";
+
+interface UserSelectRowProps {
+  user: User;
+  isSelected: boolean;
+  onSelect: (id: string) => void;
+}
+
+const iconName = (isSelected: boolean) => {
+  return isSelected ? "check_box" : "check_box_outline_blank";
+};
+
+const className = (isSelected: boolean) => {
+  return isSelected ? "text-secondary-700" : "text-gray-900";
+};
+
+const bgColor = (isSelected: boolean) => {
+  return isSelected ? "secondary" : "white";
+};
+
+const UserSelectRow = ({ user, isSelected, onSelect }: UserSelectRowProps) => {
+  const checkBox = (isSelected: boolean) => {
+    return (
+      <span
+        className={
+          "icons-default icons-filled cursor-pointer font-light " +
+          className(isSelected)
+        }
+        onClick={() => {
+          onSelect(user.id);
+        }}
+      >
+        {iconName(isSelected)}
+      </span>
+    );
+  };
+  return (
+    <UserRow
+      user={user}
+      right={checkBox(isSelected)}
+      bgColor={bgColor(isSelected)}
+    />
+  );
+};
+
+export default UserSelectRow;
---
+++ src/components/common/user/_atomic/UserRow.tsx
@@ -0,0 +1,40 @@
+import type { User } from "@prisma/client";
+import React from "react";
+
+interface UserRowProps {
+  user: User;
+  bgColor?: "white" | "secondary";
+  right?: React.ReactNode;
+}
+
+const backgroundColor = (bgColor: "white" | "secondary") => {
+  return bgColor === "white" ? "bg-white" : "bg-secondary-100";
+};
+
+const UserRow = ({ user, bgColor = "white", right = null }: UserRowProps) => {
+  return (
+    <div
+      className={
+        "select-none flex flex-row items-center p-[16px] border-b-[1px] border-gray-100 justify-between " +
+        backgroundColor(bgColor)
+      }
+    >
+      <div className="flex flex-row gap-[8px] items-center">
+        <div className="bg-gray-500 text-gray-100 rounded-[50px] w-[32px] h-[32px] items-center justify-center flex">
+          <span className="icons-default icons-filled text-[32px] font-black ">
+            person
+          </span>
+        </div>
+        <div className="flex flex-col">
+          <p className="text-[14px] font-semibold">{user.nickname}</p>
+          <p className="text-[12px] font-regular text-gray-900">
+            @{user.loginId}
+          </p>
+        </div>
+      </div>
+      {right}
+    </div>
+  );
+};
+
+export default UserRow;
