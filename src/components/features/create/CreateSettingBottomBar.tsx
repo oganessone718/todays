0508d7335f12dd3b29b313b63ea263ed885ab0bc
oganessone718 <oganessone718@gmail.com>
2025-08-04 00:52:23 +0900

    fix: add left component to bottom bar

--- src/components/features/create/CreateSettingBottomBar.tsx
+++ src/components/features/create/CreateSettingBottomBar.tsx
@@ -1,21 +1,26 @@
 "use client";
 
 import Button from "@/components/common/button/Button";
+import React from "react";
 
 interface CreateSettingsBottomBarProps {
+  left?: React.ReactNode;
   buttonText: string;
   onClick: () => void;
 }
 
 const CreateSettingsBottomBar = ({
+  left = null,
   buttonText,
   onClick,
 }: CreateSettingsBottomBarProps) => {
-
   return (
     <div>
-      <div className="flex justify-end border-t-[1px] border-gray-300 bg-white fixed bottom-0 w-full px-[16px] py-[10px] gap-[12px] z-50">
-        <Button text={buttonText} onClick={onClick} />
+      <div className="flex border-t-[1px] border-gray-300 bg-white fixed bottom-0 w-full px-[16px] py-[10px] gap-[12px] z-50 items-center">
+        {left}
+        <div className="ml-auto">
+          <Button text={buttonText} onClick={onClick} />
+        </div>
       </div>
       {/* spacer */}
       <div className="h-[57px]"></div>
