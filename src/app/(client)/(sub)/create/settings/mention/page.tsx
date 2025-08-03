3647d09fb397993bd75534a961c350eae21a2cb6
oganessone718 <oganessone718@gmail.com>
2025-08-04 00:55:38 +0900

    fix: remove searchText after clicking a back button

--- src/app/(client)/(sub)/create/settings/mention/page.tsx
+++ src/app/(client)/(sub)/create/settings/mention/page.tsx
@@ -108,6 +108,8 @@ const CreateSettingsMention = () => {
               iconName="arrow_left_alt"
               onClick={() => {
                 setIsSearchOpen(false);
+                setTmpSearchText("")
+                setSearchText("")
               }}
             />
           }
