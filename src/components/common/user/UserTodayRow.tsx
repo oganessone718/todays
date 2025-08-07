07f392f6167673f86a40ea5d2bcf4f70e06bd4e7
oganessone718 <oganessone718@gmail.com>
2025-08-08 04:05:01 +0900

    feat: impl user today row

---
+++ src/components/common/user/UserTodayRow.tsx
@@ -0,0 +1,31 @@
+import IconButton from "@/components/common/button/IconButton";
+import VisibilityTag from "@/components/common/tag/VisibilityTag";
+import { User } from "@prisma/client";
+import UserRow from "./_atomic/UserRow";
+
+interface UserTodayRowProps {
+  user: User;
+  visibility: "all" | "some" | "none";
+  isMine: boolean;
+  onClick: () => void;
+}
+
+const UserTodayRow = ({
+  user,
+  visibility,
+  isMine,
+  onClick,
+}: UserTodayRowProps) => {
+  const VisibilityAndMenu = () => {
+    return (
+      <div className="flex flex-row gap-[4px] items-center">
+        {visibility !== "all" && <VisibilityTag visibilityType={visibility} />}
+        {isMine && <IconButton iconName="more_vert" onClick={onClick} />}
+      </div>
+    );
+  };
+
+  return <UserRow user={user} right={VisibilityAndMenu()} />;
+};
+
+export default UserTodayRow;
